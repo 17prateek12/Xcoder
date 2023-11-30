856906c626bf3b9e2370e163c1000d1e72d11392
17prateek12 <88935432+17prateek12@users.noreply.github.com>
2023-12-01 00:36:39 +0530

    Update SuccesStory.tsx

--- src/app/component/SuccesStory.tsx
+++ src/app/component/SuccesStory.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Brightness1OutlinedIcon from '@mui/icons-material/Brightness1Outlined';
 import CircleIcon from '@mui/icons-material/Circle';
-
 
 const SuccesStory = () => {
     return (
@@ -10,17 +9,17 @@ const SuccesStory = () => {
             <p className='text-5xl text-blue-800 mt-10 text-center font-bold'>See Our Success Stories</p>
             <div className='flex justify-center items-center mt-44'>
                 <Avatar className='bg-blue-700 mr-6'>
-                    <img src='/Fleft.png' alt='...'/>
+                    <img src='/Fleft.png' alt='Left Avatar' />
                 </Avatar>
                 <div className='flex justify-start h-64 w-1/2'>
                     <div className='w-2/12 bg-blue-600 rounded-l-3xl'>
-                        <img src='/fff.png' className='mt-24 ml-14' alt='...' />
+                        <img src='/fff.png' className='mt-24 ml-14' alt='Decorative Image' />
                     </div>
                     <div className='flex w-10/12 box shadow-2xl rounded-r-3xl'>
                         <div className='w-3/4'>
                             <p className='text-lg ml-6 mt-6'>There are many variations of passages of Lorem Ipsum available
-                                but the majority have sufferg alteration in some form, by injected humour, or
-                                randomised words which don't look even slightly believable. If you are going to
+                                but the majority have suffered alteration in some form, by injected humour, or
+                                randomised words which don&apos;t look even slightly believable. If you are going to
                                 use a passage.</p>
                             <p className='mt-16 text-end font-bold'>-Abcdefgh Ijklmnop </p>
                             <p className='text-end'>Frontend Engineer</p>
@@ -29,7 +28,7 @@ const SuccesStory = () => {
                     </div>
                 </div>
                 <Avatar className='bg-blue-700 ml-6'>
-                    <img src='/Fright.png' alt='...'/>
+                    <img src='/Fright.png' alt='Right Avatar' />
                 </Avatar>
             </div>
             <div className='flex justify-center mt-8 mx-auto'>
@@ -38,7 +37,7 @@ const SuccesStory = () => {
                 <CircleIcon className='h-5 w-5 fill-gray-400' />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SuccesStory
+export default SuccesStory;
